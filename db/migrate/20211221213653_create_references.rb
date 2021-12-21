3ff2c291f9e85ea7a2c152c889aed655c3d39e51
Sara Kahankova <75204851+kahinek1999@users.noreply.github.com>
2021-12-21 15:52:56 -0600

    models modified

--- db/migrate/20211221213653_create_references.rb
+++ db/migrate/20211221213653_create_references.rb
@@ -2,7 +2,7 @@ class CreateReferences < ActiveRecord::Migration[6.1]
   def change
     create_table :references do |t|
       t.string :name
-      t.string :content
+      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_12_21_214639) do
 
   create_table "references", force: :cascade do |t|
     t.string "name"
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
