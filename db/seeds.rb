4d6ba5cf5661d40204e3eef3de2cfb8354376a6e
Sara Kahankova <75204851+kahinek1999@users.noreply.github.com>
2021-12-22 18:21:34 -0600

    seeds and readme update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Post.destroy_all
+Service.destroy_all
+Reference.destroy_all
+Rental.destroy_all
+
+puts "Creating posts..."
+
+3.times do
+  post = Post.create(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words
+  )
+
+  puts "Created #{post.id}..."
+end
+
+puts "Creating rentals..."
+
+5.times do
+  rental = Rental.create(
+    title: Faker::Book.title,
+    address: Faker::Address.street_address,
+    description: Faker::Quote.famous_last_words,
+    price: rand(5000..50000),
+    size: "#{rand(1..5)} + kk",
+    city: Faker::Address.city
+  )
+
+  puts "Created #{rental.id}..."
+end
+
+puts "Creating references..."
+
+10.times do
+  reference = Reference.create(
+    name: Faker::Name.name,
+    content: Faker::Quote.famous_last_words,
+  )
+
+  puts "Created #{reference.id}..."
+end
+
+puts "Creating services..."
+
+3.times do
+  service = Service.new(
+    name: Faker::Book.title,
+    description: Faker::Quote.famous_last_words,
+    price: rand(1000..15_000),
+  )
+
+  service.references = Reference.all.sample
+  service.save
+
+  puts "Created #{service.id}..."
+end
